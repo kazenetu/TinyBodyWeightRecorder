dacb4ec5d74d97b892fa3641c801fc6f7b6043be
kazenetu <rakudafactory@gmail.com>
2018-02-11 22:44:12 +0900

    グラフウィンドウを追加

---
+++ TinyBodyWeightRecorder/GraphView.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace TinyBodyWeightRecorder
+{
+    partial class GraphView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.HorizontalLineAnnotation horizontalLineAnnotation2 = new System.Windows.Forms.DataVisualization.Charting.HorizontalLineAnnotation();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.targetDateFrom = new System.Windows.Forms.DateTimePicker();
+            this.targetDateTo = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.search = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.chart1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(549, 454);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.search);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.targetDateTo);
+            this.panel1.Controls.Add(this.targetDateFrom);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(543, 39);
+            this.panel1.TabIndex = 0;
+            // 
+            // chart1
+            // 
+            horizontalLineAnnotation2.Name = "HorizontalLineAnnotation1";
+            this.chart1.Annotations.Add(horizontalLineAnnotation2);
+            chartArea2.AxisX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Days;
+            chartArea2.AxisY.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea2.AxisY2.Title = "体重";
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chart1.Location = new System.Drawing.Point(3, 48);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Chocolate;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(543, 403);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // targetDateFrom
+            // 
+            this.targetDateFrom.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.targetDateFrom.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.targetDateFrom.Location = new System.Drawing.Point(9, 9);
+            this.targetDateFrom.Name = "targetDateFrom";
+            this.targetDateFrom.Size = new System.Drawing.Size(107, 19);
+            this.targetDateFrom.TabIndex = 1;
+            this.targetDateFrom.Value = new System.DateTime(2018, 2, 10, 0, 0, 0, 0);
+            // 
+            // targetDateTo
+            // 
+            this.targetDateTo.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.targetDateTo.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.targetDateTo.Location = new System.Drawing.Point(145, 9);
+            this.targetDateTo.Name = "targetDateTo";
+            this.targetDateTo.Size = new System.Drawing.Size(107, 19);
+            this.targetDateTo.TabIndex = 1;
+            this.targetDateTo.Value = new System.DateTime(2018, 2, 10, 0, 0, 0, 0);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "～";
+            // 
+            // search
+            // 
+            this.search.Location = new System.Drawing.Point(274, 9);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(75, 23);
+            this.search.TabIndex = 3;
+            this.search.Text = "検索";
+            this.search.UseVisualStyleBackColor = true;
+            this.search.Click += new System.EventHandler(this.search_Click);
+            // 
+            // GraphView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 454);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "GraphView";
+            this.Text = "グラフ";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.GraphView_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button search;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker targetDateTo;
+        private System.Windows.Forms.DateTimePicker targetDateFrom;
+    }
+}
---
+++ TinyBodyWeightRecorder/GraphView.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using TinyBodyWeightRecorder.Models;
+
+namespace TinyBodyWeightRecorder
+{
+    public partial class GraphView : Form
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public GraphView()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// ロードイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GraphView_Load(object sender, EventArgs e)
+        {
+            // 体重情報コレクションクラスを取得
+            var bodyWights = BodyWights.GetInstance();
+
+            // 検索日付コントロールの設定
+            targetDateFrom.Value = bodyWights.Min(item=>item.WeighingDate).Date;
+            targetDateTo.Value = bodyWights.Max(item => item.WeighingDate).Date;
+
+            // 検索ボタンクリックイベント実行
+            search.PerformClick();
+
+            // 検索ボタンをアクティブに設定
+            search.Focus();
+        }
+
+        /// <summary>
+        /// 検索ボタンクリック
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void search_Click(object sender, EventArgs e)
+        {
+            // 体重情報コレクションクラスを取得
+            var bodyWights = BodyWights.GetInstance();
+
+            // 検索日付コントロールを日付情報を取得
+            var targetFrom = targetDateFrom.Value.Date;
+            var targetTo = targetDateTo.Value.Date;
+
+            // グラフ表示
+            drawGraph(targetFrom, targetTo);
+        }
+
+        /// <summary>
+        /// グラフ表示
+        /// </summary>
+        /// <param name="targetFrom">検索対象の開始日付</param>
+        /// <param name="targetTo">検索対象の終了日付</param>
+        private void drawGraph(DateTime targetFrom, DateTime targetTo)
+        {
+            // 体重情報コレクションクラスを取得
+            var bodyWights = BodyWights.GetInstance();
+
+            // グラフのクリア
+            chart1.Series.Clear();
+            chart1.Legends.Clear();
+            chart1.ChartAreas["ChartArea1"].AxisX.MajorGrid.Enabled = false;
+            chart1.ChartAreas["ChartArea1"].AxisY.MajorGrid.Enabled = false;
+
+            // 検索対象期間を取得
+            var targetItems = bodyWights.Where(item => item.WeighingDate >= targetFrom && item.WeighingDate <= targetTo)?.OrderBy(item => item.WeighingDate);
+            if (!targetItems.Any())
+            {
+                // ゼロ件の場合は終了
+                return;
+            }
+            
+            Series test = new Series();
+            test.ChartType = SeriesChartType.Line;
+
+            var points = test.Points;
+            var year = 0;
+            foreach (var item in targetItems)
+            {
+                var dateFormat = "MM/dd";
+                if (year != item.WeighingDate.Year)
+                {
+                    dateFormat = "yyyy/" + dateFormat;
+                    year = item.WeighingDate.Year;
+                }
+
+                points.AddXY(item.WeighingDate.ToString(dateFormat), item.Wight);
+            }
+
+            chart1.Series.Add(test);
+
+            chart1.ChartAreas["ChartArea1"].AxisY.Minimum = (double)targetItems.Min(item=>item.Wight)-1;
+            chart1.ChartAreas["ChartArea1"].AxisY.Maximum = (double)targetItems.Max(item => item.Wight)+1;
+            chart1.ChartAreas["ChartArea1"].AxisY.Interval = 0.5;
+        }
+    }
+}
